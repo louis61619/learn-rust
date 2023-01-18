d3ed9b9934dfdc244057aa066806eab12338c0ef
louis61619 <louis61619@gmail.com>
2023-01-18 13:42:35 +0800

    init

---
+++ 2_rust入門/src/2.1.1_變數可變.rs
@@ -0,0 +1,15 @@
+
+// 變數不可變
+// fn main() {
+//     let x = 5;
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+}
